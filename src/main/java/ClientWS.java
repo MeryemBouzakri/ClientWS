7890c9c6d3c87752aa4d72ffd04a6f7724e449bf
meryem3005 <bouzekrimeryem@gmail.com>
2024-10-18 21:24:31 +0100

    first commit

---
+++ src/main/java/ClientWS.java
@@ -0,0 +1,13 @@
+import proxy.BankService;
+import proxy.BanqueWS;
+import proxy.Compte;
+
+public class ClientWS {
+    public static void main(String[] args) {
+        BankService stub=new BanqueWS().getBankServicePort();
+        System.out.println(stub.convert(7600));
+        Compte cp= stub.getCompte(5);
+        System.out.println(cp.getCode());
+        System.out.println(cp.getSolde());
+    }
+}
